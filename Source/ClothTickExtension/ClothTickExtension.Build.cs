8a53b485fd3c826676a7045822f652d36f8fa3e2
shq300 <69398583+shq300@users.noreply.github.com>
2023-04-08 21:29:34 +0200

    5.0 update

--- Source/ClothTickExtension/ClothTickExtension.Build.cs
+++ Source/ClothTickExtension/ClothTickExtension.Build.cs
@@ -4,15 +4,25 @@ using UnrealBuildTool;
 public class ClothTickExtension : ModuleRules
 {
 	public ClothTickExtension(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicDependencyModuleNames.AddRange(
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        PrecompileForTargets = PrecompileTargetsType.Any;
+        bPrecompile = true;
+		bUsePrecompiled = true;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+            );
+		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core",
 				"CoreUObject",
-				"Engine"
+				"Engine",
+				"Slate",
+				"SlateCore",
 			}
 			);
 	}
